76c336de2071f6e186e4d27f09be123f7e85ebe8
MuTaToR <mutator08@mail.ru>
2020-04-22 12:23:08 +0300

    realised programm

--- module_0/main.py
+++ module_0/main.py
@@ -1,1 +1,32 @@
-print("Hello world")
+import numpy as np
+import math
+def game_core_v1(number):
+    '''Просто угадываем на random, никак не используя информацию о больше или меньше.
+       Функция принимает загаданное число и возвращает число попыток'''
+    count = 1
+    predict = 100 / 2  # середина всех значений
+    shift = 25
+    while number != predict:
+        count += 1
+        if number > predict:
+            predict += shift
+        elif number < predict:
+            predict -= shift
+        shift = math.ceil(shift / 2)
+    return (count)
+
+
+def score_game(game_core):
+    '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+    count_ls = []
+    np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+    random_array = np.random.randint(1, 101, size=(1000))
+    for number in random_array:
+        count_ls.append(game_core(number))
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+    return (score)
+
+
+# запускаем
+score_game(game_core_v1)
